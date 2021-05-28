11ccb447f6f9979f3b37ac89b5a8149812ccb182
acanhost <acanstudio@canliang.wang>
2021-05-28 19:46:46 +0800

    init system

---
+++ routes/api.php
@@ -0,0 +1,73 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use Larabase\Helpers\ResourceManager;
+
+$middlewareAuth = [
+    App\Middleware\JWTAuthMiddleware::class,
+];
+$middlewareBackend = array_merge($middlewareAuth, [
+    App\Middleware\BackendMiddleware::class,
+    App\Middleware\PermissionMiddleware::class,
+]);
+
+
+$routes = ResourceManager->initRouteDatas();
+Router::addGroup('', function () use ($middlewareBackend, $routes) {
+    foreach ($routes as $rCode => $rMethods) {
+        foreach ($rMethods as $action => $data) {
+            echo implode(',', $data['method']) . '==' . $data['path'] . '==' . $data['callback'] . "\n";
+            Router::addRoute($data['method'], $data['path'], $data['callback'], ['middleware' => $middlewareBackend, 'routeCode' => $data['code']]);
+        }
+    }
+});
+
+
+Route::match(['OPTIONS', 'GET', 'POST', 'HEAD'], '/', 'App\Controllers\IndexController@index');
+Route::get('/captcha', 'App\Controllers\CommonController@captcha');
+Route::get('/send-code', 'App\Controllers\CommonController@sendCode');
+Route::get('/validate-code', 'App\Controllers\CommonController@validateCode');
+
+Route::addRoute(['GET', 'POST'], '/logout', 'App\Controllers\EntranceController@logout');
+
+Route::post('/signup', 'App\Controllers\EntranceController@signup');
+Route::post('/signupin', 'App\Controllers\EntranceController@signupin');
+Route::post('/token', 'App\Controllers\EntranceController@token');
+Route::post('/signin', 'App\Controllers\EntranceController@signin');
+
+Route::get('/myinfo', 'App\Controllers\UserController@myinfo', ['middleware' => $middlewareAuth]);
+Route::post('/refresh_token', 'App\Controllers\EntranceController@refreshToken', ['middleware' => $middlewareAuth]);
+Route::get('/my-routes', 'App\Controllers\EntranceController@myRoutes', ['middleware' => $middlewareBackend]);
+
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+$authAttributes = [
+    'prefix' => '',
+    //'namespace' => 'auth:api',
+    'namespace' => 'Auth',
+    'middleware' => 'api',
+    //'middleware' => 'cors',
+];
+
+Route::group($authAttributes, function() {
+    Route::post('login', 'AuthController@login');//->name('login'); // 用户名、密码登录接口
+    Route::post('refresh', 'AuthController@refresh');
+    Route::get('register', 'RegisterController@register');
+    Route::match(['get', 'post'], '/action', 'Controller@action');
+    //Route::any('/action', 'Controller@action');
+});
+
+$authAttributes['middleware'] = ['api', 'auth:api'];
+
+Route::group($authAttributes, function() {
+    Route::get('myinfo', 'UserController@myinfo');
+    Route::post('logout', 'UserController@logout'); // 注销当前用户
+});
+
+
+
---
+++ routes/web.php
@@ -0,0 +1,73 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+    Route::any('/captcha-test', function() {
+        if (request()->getMethod() == 'POST') {
+            $rules = ['captcha' => 'required|captcha'];
+            //print_r(request()->all());
+            $validator = validator()->make(request()->all(), $rules);
+            if ($validator->fails()) {
+                echo '<p style="color: #ff0000;">Incorrect!</p>';
+            } else {
+                echo '<p style="color: #00ff30;">Matched :)</p>';
+            }
+        }
+    
+        $form = '<form method="post" action="captcha-test">';
+        $form .= '<input type="hidden" name="_token" value="' . csrf_token() . '">';
+        $form .= '<p>' . captcha_img() . '</p>';
+        $form .= '<p><input type="text" name="captcha"></p>';
+        $form .= '<p><button type="submit" name="check">Check</button></p>';
+        $form .= '</form>';
+        return $form;
+    });
+
+$routes = [
+    'subject' => [
+        'routes' => ['', 'human', 'info', 'knowledge', 'product', 'shop', 'store', 'league'],
+    ],
+    'brand' => [
+        'routes' => ['', 'detail', 'product', 'product', 'shop', 'store'],
+    ],
+    'pet' => [
+        'routes' => ['', 'special-list', 'special-show-{id}', 'pet-home/{code?}', 'pet-list/{code?}', 'pet-show-{id}', 'info-home/{code?}', 'info-list/{code?}', 'info-show-{id}'],
+    ],
+    'guide' => [
+        'routes' => ['', 'vote', 'show-store-{id}', 'show-knowledge-{id}', 'show-shop-{id}', 'show-league-{id}', 'show-human-{id}', 'show-info-{id}'],
+    ],
+    'human' => [
+        'routes' => ['', '{code}'],
+        '{code}' => ['action' => 'view'],
+    ],
+    'culture' => [
+        'routes' => ['', 'listinfo', 'list/{code}/{page?}', 'show-{id}', 'test'],
+        'list/{code}/{page?}' => ['action' => 'listinfo'],
+    ],
+];
+$currentHost = false;
+$currentHost = $_SERVER['HTTP_HOST'] ?? false;
+if ($currentHost) {
+    $siteCode = explode('.', $currentHost)[0];
+    $siteCode = explode('-', $siteCode)[0];
+    $currentHost = in_array($siteCode, array_keys($routes)) ? $siteCode : false;
+}
+//var_dump($currentHost);
+foreach ($routes as $domain => $domainRoutes) {
+    //if ($currentHost !== false && $currentHost != $domain) {
+    if ($currentHost != $domain) {
+        continue;
+    }
+    Route::domain(config('app.' . $currentHost . 'Domain'))->group(function () use ($domain, $domainRoutes) {
+        foreach ($domainRoutes['routes'] as $route) {
+            ResourceManager::setRoute($route, $domain, $domainRoutes);
+        }
+    });
+}
+
+/*Route::get('/movies/{id}', 'MoviesController@show')->name('movies.show');
+Route::get('/tv', 'TvController@index')->name('tv.index');
+Route::get('/tv/{id}', 'TvController@show')->name('tv.show');
+Route::get('/actors', 'ActorsController@index')->name('actors.index');
+Route::get('/actors/page/{page?}', 'ActorsController@index');
+Route::get('/actors/{id}', 'ActorsController@show')->name('actors.show');*/
