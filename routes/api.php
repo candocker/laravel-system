83c77ccc9cc764be1d04ea02f83996f808dae0fa
acanhost <acanstudio@canliang.wang>
2021-06-17 11:03:57 +0800

    develop laravel system

--- routes/api.php
+++ routes/api.php
@@ -19,15 +19,6 @@ $middlewareBackend = array_merge($middlewareAuth, [
 ]);
 
 $routes = app()->make(ResourceContainer::class)->initRouteDatas();
-/*Router::addGroup('', function () use ($middlewareBackend, $routes) {
-    foreach ($routes as $rCode => $rMethods) {
-        foreach ($rMethods as $action => $data) {
-            echo implode(',', $data['method']) . '==' . $data['path'] . '==' . $data['callback'] . "\n";
-            Router::addRoute($data['method'], $data['path'], $data['callback'], ['middleware' => $middlewareBackend, 'routeCode' => $data['code']]);
-        }
-    }
-});*/
-
 foreach ($routes as $app => $aRoutes) {
     if ($app == 'third') {
         continue;
@@ -41,8 +32,6 @@ foreach ($routes as $app => $aRoutes) {
         }
     }
 }
-//exit();
-
 
 Route::match(['OPTIONS', 'GET', 'POST', 'HEAD'], '/', '\App\Http\Controllers\IndexController@index');
 Route::get('/passport/captcha', '\ModulePassport\Controllers\CommonController@captcha');
@@ -52,40 +41,11 @@ Route::get('/passport/validate-code', '\ModulePassport\Controllers\CommonControl
 
 Route::addRoute(['GET', 'POST'], '/passport/logout', '\ModulePassport\Controllers\EntranceController@logout');
 
-Route::post('/signup', '\ModulePassport\Controllers\EntranceController@signup');
-Route::post('/signupin', '\ModulePassport\Controllers\EntranceController@signupin');
+Route::post('/passport/signup', '\ModulePassport\Controllers\EntranceController@signup');
+Route::post('/passport/signupin', '\ModulePassport\Controllers\EntranceController@signupin');
 Route::post('/passport/token', '\ModulePassport\Controllers\EntranceController@token');
-Route::post('/signin', '\ModulePassport\Controllers\EntranceController@signin');
+Route::post('/passport/signin', '\ModulePassport\Controllers\EntranceController@signin');
 
 Route::middleware($middlewareAuth)->get('/passport/myinfo', '\ModulePassport\Controllers\UserController@myinfo');
-//Route::get('/passport/myinfo', '\ModulePassport\Controllers\UserController@myinfo');
-Route::post('/refresh_token', '\ModulePassport\Controllers\EntranceController@refreshToken', ['middleware' => $middlewareAuth]);
-Route::get('/my-routes', '\ModulePassport\Controllers\EntranceController@myRoutes', ['middleware' => $middlewareBackend]);
-
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-/*$authAttributes = [
-    'prefix' => '',
-    //'namespace' => 'auth:api',
-    'namespace' => 'Auth',
-    'middleware' => 'api',
-    //'middleware' => 'cors',
-];
-
-Route::group($authAttributes, function() {
-    Route::post('login', 'AuthController@login');//->name('login'); // 用户名、密码登录接口
-    Route::post('refresh', 'AuthController@refresh');
-    Route::get('register', 'RegisterController@register');
-    Route::match(['get', 'post'], '/action', 'Controller@action');
-    //Route::any('/action', 'Controller@action');
-});
-
-$authAttributes['middleware'] = ['api', 'auth:api'];
-
-Route::group($authAttributes, function() {
-    Route::get('myinfo', 'UserController@myinfo');
-    Route::post('logout', 'UserController@logout'); // 注销当前用户
-});*/
+Route::middleware($middlewareAuth)->post('/passport/refresh-token', '\ModulePassport\Controllers\EntranceController@refresh');
+Route::middleware($middlewareAuth)->get('/passport/my-routes', '\ModulePassport\Controllers\EntranceController@myRoutes', ['middleware' => $middlewareBackend]);
