e64d193317a2dde7cbc25ec0133246470685c8ff
acanhost <acanstudio@canliang.wang>
2021-08-02 21:00:05 +0800

    develop laravel

--- routes/culture.php
+++ routes/culture.php
@@ -4,14 +4,21 @@ use Illuminate\Support\Facades\Route;
 
 use Framework\Baseapp\Helpers\ResourceContainer;
 
-$middlewareAuth = [
-    //'auth:api',
+$middleware = [
     'cors',
-    //Framework\Baseapp\Middleware\AuthUserMiddleware::class,
+];
+$middlewareAuth = [
+    'cors',
+    Framework\Baseapp\Middleware\AuthPayloadMiddleware::class,
+    Framework\Baseapp\Middleware\UserCenterMiddleware::class,
 ];
 
-Route::middleware($middlewareAuth)->get('/culture/books/epub', '\ModuleCulture\Controllers\BookController@epub');
-Route::middleware($middlewareAuth)->get('/culture/books/home', '\ModuleCulture\Controllers\BookController@home');
-Route::middleware($middlewareAuth)->get('/culture/books/detail', '\ModuleCulture\Controllers\BookController@detail');
-Route::middleware($middlewareAuth)->get('/culture/tags/home', '\ModuleCulture\Controllers\TagController@home');
-Route::middleware($middlewareAuth)->get('/culture/chapters/epub', '\ModuleCulture\Controllers\ChapterController@epub');
+Route::middleware($middleware)->get('/culture/books/epub', '\ModuleCulture\Controllers\BookController@epub');
+Route::middleware($middleware)->get('/culture/books/home', '\ModuleCulture\Controllers\BookController@home');
+Route::middleware($middleware)->get('/culture/books/detail', '\ModuleCulture\Controllers\BookController@detail');
+Route::middleware($middleware)->get('/culture/books/front-list', '\ModuleCulture\Controllers\BookController@frontList');
+Route::middleware($middleware)->get('/culture/tags/home', '\ModuleCulture\Controllers\TagController@home');
+Route::middleware($middleware)->get('/culture/categories/home', '\ModuleCulture\Controllers\CategoryController@home');
+Route::middleware($middleware)->get('/culture/chapters/epub', '\ModuleCulture\Controllers\ChapterController@epub');
+Route::middleware($middlewareAuth)->any('/culture/chapter-records/record', '\ModuleCulture\Controllers\ChapterRecordController@record');
+Route::middleware($middlewareAuth)->any('/culture/chapter-records/record1', '\ModuleCulture\Controllers\ChapterRecordController@record');
