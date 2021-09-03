caff4f3dfb6228f822a47e879539ccd3d0d9927e
acanhost <acanstudio@canliang.wang>
2021-09-03 13:34:37 +0800

    develop laravel

--- routes/bigdata.php
+++ routes/bigdata.php
@@ -10,6 +10,7 @@ $middlewareAuth = [
     //Framework\Baseapp\Middleware\AuthUserMiddleware::class,
 ];
 
-Route::middleware($middlewareAuth)->get('/bigdata/data-sync', '\ModuleBigdata\Controllers\DataSyncController@sync');
-Route::middleware($middlewareAuth)->get('/bigdata/sync-status', '\ModuleBigdata\Controllers\DataSyncController@syncStatus');
+Route::middleware($middlewareAuth)->get('/bigdata/deal-order', '\ModuleBigdata\Controllers\DataSyncController@dealOrder');
+Route::middleware($middlewareAuth)->get('/bigdata/update-sync', '\ModuleBigdata\Controllers\DataSyncController@updateSync');
 Route::middleware($middlewareAuth)->get('/bigdata/sync-dump', '\ModuleBigdata\Controllers\DataSyncController@syncDump');
+Route::middleware($middlewareAuth)->get('/bigdata/table-structure', '\ModuleBigdata\Controllers\DataSyncController@tableStructure');
